2096dce27410ba1fef8f0b00c1af73ffd998e5be
Oleg Konovalov V <konovalov_o@vk.com>
2025-07-01 22:11:30 +0300

    custom component - add custom rest method

---
+++ local/modules/ok.crmtab/lang/ru/install/index.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['OK_CRMTAB_INSTALL_MODULE_DESCRIPTION'] = 'Модуль добавления табов в карточки сделки';
+$MESS['OK_CRMTAB_INSTALL_MODULE_NAME'] = 'Таб в сделке';
+$MESS['OK_CRMTAB_PARTNER_NAME'] = 'Коновалов Олег';
+$MESS['OK_CRMTAB_PARTNER_URI'] = 'https://okonovalov.ru';
